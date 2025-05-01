53202ed40d17ad5178282a8a9bcdc859ff95b2ca
Bravinsimiyu <bravinwasike18@gmail.com>
2025-05-01 18:59:06 +0300

    minor changes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get("/", getProducts);
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.delete("/:id", deleteProduct)
 
 export default router;
